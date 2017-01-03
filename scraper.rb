cc76769838544af69a801227ab6b7c9c87b00f3f
Tony Bowden <tony@tmtm.com>
2017-01-03 16:11:50 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -11,12 +12,12 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_list(termid, url)
@@ -28,26 +29,26 @@ def scrape_list(termid, url)
       info = mp.css('div.personinfo')
       next if mp.xpath('.//preceding::h2').count == 2
       next if mp.css('h3').text.empty?
-      data = { 
-        id: mp.css('div.overlaybox/@data-item').text,
-        name: mp.css('h3').text.tidy,
-        image: mp.css('.imagebox img/@src').text,
-        email: info.css('.iemail/@dataitem').text,
-        party: info.css('p')[1].text,
+      data = {
+        id:       mp.css('div.overlaybox/@data-item').text,
+        name:     mp.css('h3').text.tidy,
+        image:    mp.css('.imagebox img/@src').text,
+        email:    info.css('.iemail/@dataitem').text,
+        party:    info.css('p')[1].text,
         party_id: info.css('p')[1].text,
-        region: region.text,
-        term: termid,
-        source: url,
+        region:   region.text,
+        term:     termid,
+        source:   url,
       }
-      count+=1
+      count += 1
       # puts data
-      ScraperWiki.save_sqlite([:name, :term], data)
+      ScraperWiki.save_sqlite(%i(name term), data)
     end
   end
   puts "Added #{count}"
 end
 
-terms = { 
+terms = {
   '2013-2017' => 'http://www.landtag.li/personen.aspx?nid=4158&auswahl=4158&lang=de',
   '2009-2013' => 'http://www.landtag.li/personen.aspx?nid=4158&auswahl=4158&lang=de&jahr=2009&sitzordnung=0',
   '2005-2009' => 'http://www.landtag.li/personen.aspx?nid=4158&auswahl=4158&lang=de&jahr=2005&sitzordnung=0',
